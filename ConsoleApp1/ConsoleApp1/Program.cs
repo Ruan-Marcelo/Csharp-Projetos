d7e63fbbe6ce0b6dfe5c3363e498906dceaff5c5
RUAN MARCELO RAMACIOTI LUZ <luzruan60@gmail.com>
2026-03-26 13:45:49 -0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿string teclado;
+int soma;
+
+
+for(int i = 0; i < 10; i++)
+{
+    Console.WriteLine("Digite um número que irei fazer a soma");
+    teclado = Console.ReadLine();
+    soma = Convert.ToInt32(teclado);
+    Console.WriteLine($"A soma total dos números é de {soma + soma}");
+}
